f79aa107d6b51fbce1dff087378dfece4a872bfb
Rodney Omukuti <98819046+Rodneyomukuti@users.noreply.github.com>
2026-04-19 11:09:26 +0300

    Add village games custom tasks (#10)
    * Add village games custom tasks
    
    * Update file
    
    * Update task to include report rendering
    
    * Update pyproject toml file
    
    * Fix commit errors
    
    * Update file
    
    * update spec.yaml
    
    * Update dashboard layout
    
    * Build and compile

--- src/ecoscope-workflows-ext-bh-village-games/ecoscope_workflows_ext_bh_village_games/tasks/__init__.py
+++ src/ecoscope-workflows-ext-bh-village-games/ecoscope_workflows_ext_bh_village_games/tasks/__init__.py
@@ -1,5 +1,24 @@
+# src/ecoscope_workflows_ext_bahari_hai/tasks/__init__.py
+
 from ._example import add_one_thousand
+from ._village_games import (
+    draw_community_feedback_table,
+    draw_monthly_heatmap,
+    draw_village_donut_chart,
+    draw_activity_leaderboard,
+    draw_village_icon_bar,
+    draw_all_villages_icon_bar,
+    render_vg_report,
+)
+
 
 __all__ = [
     "add_one_thousand",
+    "draw_community_feedback_table",
+    "draw_monthly_heatmap",
+    "draw_village_donut_chart",
+    "draw_activity_leaderboard",
+    "draw_village_icon_bar",
+    "draw_all_villages_icon_bar",
+    "render_vg_report",
 ]
